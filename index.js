0681c4abf9f99fdcad3ab62cfa1f6e34a3aae401
iamkibeh <kibet.immanuel@student.moringaschool.com>
2022-08-18 07:02:39 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1=1;
+let num2=62;
+const multiply = num1*num2;
+
+const random = Math.floor(Math.random())+1;
+
+let num3 = 22;
+let num4 = 9;
+const mod = num3% num4
+
+const max = Math.max(1,2,7,20)
